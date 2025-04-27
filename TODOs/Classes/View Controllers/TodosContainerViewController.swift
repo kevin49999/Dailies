9b25d043b27a08d0ab048c370d1bd2a72feb440d
Kevin Johnson <kevinjohnsonutx@gmail.com>
2025-04-27 17:03:15 -0400

    misc2

--- TODOs/Classes/View Controllers/TodosContainerViewController.swift
+++ TODOs/Classes/View Controllers/TodosContainerViewController.swift
@@ -24,7 +24,7 @@ class TodosContainerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         observeNotifications()
-        let gearImage = UIImage(systemName: "gear")
+        let gearImage = UIImage(systemName: "gear")?    .withConfiguration(UIImage.SymbolConfiguration(scale: .medium))
         settingsBarButtonItem.image = gearImage
         add(daysOfWeekTodoController, to: contentView)
     }
