46bfab8cdfdc3f5e107a2b1a516ea52be0b7eaea
Kevin Johnson <kevinjohnsonutx@gmail.com>
2025-06-05 21:45:39 -0400

    infovc + 1.1.2 (3)

---
+++ TODOs/Classes/View Controllers/InfoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  InfoViewController.swift
+//  TODOs
+//
+//  Created by Kevin Johnson on 6/5/25.
+//  Copyright © 2025 Kevin Johnson. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UITableViewController {
+    static let githubUrl = URL(string: "https://github.com/kevin49999/Dailies")!
+
+    @IBOutlet weak private var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 92
+        tableView.rowHeight = UITableView.automaticDimension
+        versionLabel.text = Bundle.main.versionNumberString
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension InfoViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            tableView.deselectRow(at: indexPath, animated: true)
+            UIApplication.shared.open(Self.githubUrl)
+        default:
+            assertionFailure()
+        }
+    }
+}
+
--- TODOs/Extensions/Bundle+Extensions.swift
+++ TODOs/Extensions/Bundle+Extensions.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 extension Bundle {
+    var releaseVersionNumber: String? { infoDictionary?["CFBundleShortVersionString"] as? String }
+    var buildVersionNumber: String? { infoDictionary?["CFBundleVersion"] as? String }
+    var versionNumberString: String { "\(releaseVersionNumber ?? "") (\(buildVersionNumber ?? ""))" }
+    
     static func loadNibView<T: UIView>() -> T {
         guard let view = Bundle.main.loadNibNamed(T.storyboardIdentifier, owner: nil, options: nil)?.first as? T else {
             fatalError("Could not load nib view with identifier: \(T.storyboardIdentifier)")
